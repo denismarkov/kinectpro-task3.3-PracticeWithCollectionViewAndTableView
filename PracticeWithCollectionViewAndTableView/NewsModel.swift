3cac674ca0c83a6f67ca1322c149001df83ea512
Denis Markov <keynsianets@gmail.com>
2018-07-27 15:01:00 +0300

    Add .gitignore file

---
+++ PracticeWithCollectionViewAndTableView/NewsModel.swift
@@ -0,0 +1,9 @@
+//
+//  NewsModel.swift
+//  PracticeWithCollectionViewAndTableView
+//
+//  Created by Denis Markov on 7/27/18.
+//  Copyright © 2018 Denis Markov. All rights reserved.
+//
+
+import Foundation
---
+++ PracticeWithCollectionViewAndTableView/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  PracticeWithCollectionViewAndTableView
+//
+//  Created by Denis Markov on 7/27/18.
+//  Copyright © 2018 Denis Markov. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
